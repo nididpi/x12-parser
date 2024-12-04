9720933382aae9973749bcb7587781f6317a9757
wei.wu <wei.wu@abacusinsights.com>
2024-12-04 11:21:59 -0500

    POC

--- src/main/java/com/imsweb/x12/util/BuildSqlSelect.java
+++ src/main/java/com/imsweb/x12/util/BuildSqlSelect.java
@@ -13,6 +13,8 @@ import java.util.List;
 public class BuildSqlSelect {
 
     private TransactionDefinition transactionDef;
+    private String parentStatement; // Class-level variable to store the parent statement
+
 
     public BuildSqlSelect(TransactionDefinition transactionDef) {
         this.transactionDef = transactionDef;
@@ -59,13 +61,17 @@ public class BuildSqlSelect {
 
                 } else if ("2300".equals(childLoopDef.getXid()) && "2000C".equals(loopDef.getXid())) {
 
-                    exploded_alias = "exploded_" + childLoopDef.getXid().toLowerCase();
+                    exploded_alias = "exploded_prepared_" + childLoopDef.getXid().toLowerCase();
 
                     selectBuilder.append("case when exploded_isa_loop_gs_loop_st_loop_detail_2000a_2000b.2300_claim_information " +
-                            "is not null then EXPLODE_OUTER(exploded_isa_loop_gs_loop_st_loop_detail_2000a_2000b.2300_claim_information) " +
-                                "else EXPLODE_OUTER(exploded_isa_loop_gs_loop_st_loop_detail_2000a_2000b_2000C.2300_claim_information) end AS ")
+                            "is not null then exploded_isa_loop_gs_loop_st_loop_detail_2000a_2000b.2300_claim_information " +
+                                "else exploded_isa_loop_gs_loop_st_loop_detail_2000a_2000b_2000C.2300_claim_information end AS ")
                         .append(exploded_alias)
                         .append(",\n");
+
+                    buildParentStatement(loopDef, childLoopDef, exploded_alias, aliasTrail);
+
+                    continue;
 
                 } else {
                     String exploded_column_name = parentAlias + "." + childLoopDef.getXid() + "_" + childLoopDef.getName().replace(' ', '_').replaceAll("[^a-zA-Z0-9_]", "").toLowerCase();
@@ -98,6 +104,19 @@ public class BuildSqlSelect {
         return selectBuilder.toString();
     }
 
+    private void buildParentStatement(LoopDefinition loopDef, LoopDefinition childLoopDef, String parentAlias, String aliasTrail) {
+        // Build the SQL for the child loop using buildLoopSelect
+
+        String Select = "select *, explode_outer(exploded_prepared_2300) as exploded_2300, \n";
+        Select = Select + buildLoopSelect(childLoopDef, "exploded_2300", "");
+
+        this.parentStatement = Select;
+    }
+
+    public String getParentStatement() {
+        return this.parentStatement;
+    }
+
     public static void main(String[] args) {
         try {
             X12Reader reader837 = new X12Reader(X12Reader.FileType.ANSI837_5010_X222, new File("837sample"));
@@ -105,10 +124,18 @@ public class BuildSqlSelect {
             TransactionDefinition transactionDef = reader837.getDefinition();
             BuildSqlSelect builder = new BuildSqlSelect(transactionDef);
 
-            String sqlSelect = builder.buildSelectStatement();
+            String childSqlSelect = builder.buildSelectStatement();
 
-            sqlSelect = "(select " + sqlSelect + "parsed_json.Errors,\nparsed_json.FatalErrors) su";
-            System.out.println(sqlSelect);
+            childSqlSelect = "(select " + childSqlSelect + "parsed_json.Errors,\nparsed_json.FatalErrors from xjson) su";
+//            System.out.println(sqlSelect);
+
+            String sqlParentQuery = builder.getParentStatement();
+            sqlParentQuery =  sqlParentQuery.endsWith(",\n") ? sqlParentQuery.substring(0, sqlParentQuery.length() - 2) + "\n" : sqlParentQuery;
+
+//            System.out.println(sqlParentQuery);
+
+            String selectSql = sqlParentQuery + " from \n" + childSqlSelect;
+            System.out.println(selectSql);
 
         } catch (IOException e) {
             e.printStackTrace();
